1d2dccb0892486660850586ecd293b0999f2289a
AHCoderCode <vision.ah13@gmail.com>
2026-08-17 22:29:46 +0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
 // Vexel Supabase client – using anon key (safe for browser)
-const SUPABASE_URL = 'vexelai-zeta.vercel.app'; // e.g. https://abcdefgh.supabase.co
-const SUPABASE_ANON_KEY = 'sb_publishable_DNFvnqkMtzuZX2ySAmmtTg_D1kLYO9G'; // starts with eyJ...
+const SUPABASE_URL = 'const SUPABASE_URL = const SUPABASE_URL = 'https://fevlfmlcfdwgpvabjzql.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZldmxmbWxjZmR3Z3B2YWJqenFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY5NzQ2MjksImV4cCI6MjEwMjU1MDYyOX0.Fs0r4pwWPxfvHiMGDMyw5AFnjDo__iMY3ib49yMfLZM'; // starts with eyJ...
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
